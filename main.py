008a2fe2138d766c4e858dc1605f25837fc451a7
Biswajit Samal <biswajitsamal@Biswajits-MacBook-Air.local>
2026-01-05 10:51:46 +0530

    Add vector search and create agent

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from langchain_chroma import Chroma
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 import bs4
+from uuid import uuid4
+from langchain_chroma import Chroma
+from langchain.tools import tool
+from langchain.agents import AgentState, create_agent
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from local_secrets import GOOGLE_API_KEY
 
 embeddings = GoogleGenerativeAIEmbeddings(api_key=GOOGLE_API_KEY, model="models/gemini-embedding-001")
 model = ChatGoogleGenerativeAI(api_key=GOOGLE_API_KEY, model="gemini-2.5-flash", temperature=1)
 
-import bs4
-from langchain_community.document_loaders import WebBaseLoader
-
 # Only keep post title, headers, and content from the full HTML.
 bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
 loader = WebBaseLoader(
     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
     bs_kwargs={"parse_only": bs4_strainer},
-)\
+)
  
 docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
 
 assert len(docs) == 1
 print(f"Total characters: {len(docs[0].page_content)}")
@@ -29,3 +33,33 @@ vector_store = Chroma(
     persist_directory="./chroma_langchain_db",
 )
 
+uuids = [str(uuid4()) for _ in range(len(all_splits))]
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits, ids=uuids)
+
+# Construct a tool for retrieving context
+@tool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+tools = [retrieve_context]
+# If desired, specify custom instructions
+prompt = (
+    "You have access to a tool that retrieves context from a blog post. "
+    "Use the tool to help answer user queries."
+)
+agent = create_agent(model, tools, system_prompt=prompt)
+
+query = "What is task decomposition?"
+for step in agent.stream(
+    {"messages": [{"role": "user", "content": query}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
